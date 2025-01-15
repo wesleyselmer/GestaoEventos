df7e99ff1f6299db1c0b6bbb4f34a94bd7191874
Wesley <wesleyselmer@gmail.com>
2025-01-15 14:55:42 -0300

    Config Swagger

---
+++ src/MongoDbContext.cs
@@ -0,0 +1,17 @@
+using MongoDB.Driver;
+
+public class MongoDbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbContext(string connectionString, string databaseName)
+    {
+        var client = new MongoClient(connectionString);
+        _database = client.GetDatabase(databaseName);
+    }
+
+    public IMongoCollection<T> GetCollection<T>(string name)
+    {
+        return _database.GetCollection<T>(name);
+    }
+}
